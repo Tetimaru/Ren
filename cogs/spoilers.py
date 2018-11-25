c5ead0b204d57511a66cb8fba03a3bac2b5a43d9
Tetimaru <Tetimaru@users.noreply.github.com>
2018-11-25 15:17:11 -0800

    Fix pylint issues

--- cogs/spoilers.py
+++ cogs/spoilers.py
@@ -78,8 +78,8 @@ class Spoilers: # pylint: disable=too-many-instance-attributes
                 if data.type == 'image':
                     store[KEY_EMBED] = data.url
             else:
-                pattern_imglink = r"(?i)http[^ ]+\.(?:png|jpg|jpeg|gif)"
-                match = re.search(pattern_imglink, msg)
+                imglinkPattern = r"(?i)http[^ ]+\.(?:png|jpg|jpeg|gif)"
+                match = re.search(imglinkPattern, msg)
                 if match:
                     store[KEY_EMBED] = match.group(0)
             await self.bot.delete_message(ctx.message)
